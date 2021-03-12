e5d866871c142361f20e7e1eb3135eb377d7d121
barr3 <barre@novigrad.localadmin>
2021-03-13 00:20:21 +0100

    Created bot

---
+++ main.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#bot.py
+import os
+import random
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+#voice = discord.VoiceClient(client, "General")
+
+
+
+joydip_nr = 1
+
+
+def int_to_roman(input):
+    """ Convert an integer to a Roman numeral. """
+
+    ints = (1000, 900,  500, 400, 100,  90, 50,  40, 10,  9,   5,  4,   1)
+    nums = ('M',  'CM', 'D', 'CD','C', 'XC','L','XL','X','IX','V','IV','I')
+    result = []
+    for i in range(len(ints)):
+        count = int(input / ints[i])
+        result.append(nums[i] * count)
+        input -= ints[i] * count
+    return ''.join(result)
+
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    moise = [
+        'Moise är svart ngl ngl',
+        'moise moise moise moise moise moise moise miose mmiose miose moise moise moise mosie moise moise',
+        'mowshe',  
+    ]
+
+    michael = [
+        'Michael Sportingsawn',
+        'Michael Hawkingson',
+        'Michael Rasmusken',
+    ]
+
+    barre = [
+        'Jag ska inte ljuga, han den där Barre är fan hella sexig.',
+        'Tjejer akta er för Barre, han är för söt ngl fr fr',
+        'Även fast Barre är lite moisig i skallen är han fortfarande cool ngl',
+        'Fr fr visste du att Barre heter Barre Nils Fredrik Svante Åke Blomber',
+        ]
+    
+
+    if "moise" in message.content.lower():
+        #voice.connect(True, 1000)
+        response = random.choice(moise)
+        await message.channel.send(response)
+
+    if "michael" in message.content.lower():
+        response = random.choice(michael)
+        await message.channel.send(response)        
+
+    if "barre" in message.content.lower():
+        response = random.choice(barre)
+        await message.channel.send(response)                
+        
+    if "joydip" in message.content.lower():
+
+        global joydip_nr
+        
+        response = "Joydip Paul " + str(int_to_roman(joydip_nr))
+        joydip_nr += 1
+        
+        await message.channel.send(response)                
+
+        
+    
+
+# await connect(*, timeout=60.0, reconnect=True, cls=<class 'discord.voice_client.VoiceClient'>  )
+
+
+#@client.command()
+#async def join(ctx):
+#    channel = ctx.author.voice.channel
+#    await channel.connect()
+#@client.command()
+#async def leave(ctx):
+#    await ctx.voice_client.disconnect()
+
+#voice.
+
+client.run(TOKEN)
