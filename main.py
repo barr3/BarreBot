22453f02333f3b19060f6892d4c6428180d895c8
barr3 <barre@novigrad.localadmin>
2021-03-16 11:15:49 +0100

    Added bot tracking or smth

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user or message.bot == True:
         return
 
 
